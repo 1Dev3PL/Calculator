ca48419f72fc6f218a2b3a3615a8fd11b769313c
1Dev3PL <Ivan3nd3r11@gmail.com>
2019-11-23 13:08:02 +0300

    Updates

---
+++ precache-manifest.7eb769f840d82bf1db0aa5408a0e19dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b755614e2b4a437b6f12a3b81cab88ee",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "98fdaa79092f21bdb84b",
+    "url": "/Calculator/static/css/main.3367bc00.chunk.css"
+  },
+  {
+    "revision": "92317cfb55362404492b",
+    "url": "/Calculator/static/js/2.fb93d939.chunk.js"
+  },
+  {
+    "revision": "98fdaa79092f21bdb84b",
+    "url": "/Calculator/static/js/main.7c6908ef.chunk.js"
+  },
+  {
+    "revision": "948835a5ae24ee819954",
+    "url": "/Calculator/static/js/runtime-main.2f606aad.js"
+  }
+]);
